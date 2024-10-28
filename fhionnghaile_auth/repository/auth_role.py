0602ba20a008429e67301250c99cc0fac784bd0d
Kevin McGinley <kevinmcginley@cityswift.com>
2024-10-28 12:40:09 +0000

    inf_auth_role

--- fhionnghaile_auth/repository/auth_role.py
+++ fhionnghaile_auth/repository/auth_role.py
@@ -3,7 +3,7 @@ from sqlalchemy.dialects.postgresql import UUID
 
 class AuthRole(db.Model):
     __tablename__ = 'inf_auth_role'
-    role = db.Column(db.String(100), primary_key=True)
+    auth_role = db.Column(db.String(100), primary_key=True)
 
     def __init__(
         self, 
